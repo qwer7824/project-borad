cfb32d8346806fb7a2d8d2bc92121b1aad73424e
dev <sh7824@naver.com>
2022-11-15 20:18:31 +0900

    commit

---
+++ src/main/java/com/project/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,12 @@
+package com.project.projectboard.repository;
+
+import com.project.projectboard.config.JpaConfig;
+import com.project.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long>
+{
+
+}
